c9b1c57a72510b6abda43cb6736dda0b20ac9eaa
David Redding <redding.david35@gmail.com>
2021-12-15 11:19:33 -0600

    uncontrolled -> controlled

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  // "e" contains a bunch of info about the event that just occured.
-  onInputChange(e) {
-    console.log(e.target.value); // this spec contains the text, that the user just added to input.
-  }
+  // // "e" contains a bunch of info about the event that just occured.
+  // onInputChange(e) {
+  //   console.log(e.target.value); // this spec contains the text, that the user just added to input.
+  // }
+
+  state = {
+    term: "",
+  };
 
   render() {
     return (
@@ -12,7 +16,10 @@ class SearchBar extends React.Component {
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange}></input>
+            <input
+              type="text"
+              onChange={(e) => this.setState({ term: e.target.value })}
+            ></input>
           </div>
         </form>
       </div>
