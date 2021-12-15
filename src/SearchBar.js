013da6e06bc3e894ca885215827617424aa7a978
David Redding <redding.david35@gmail.com>
2021-12-15 10:23:47 -0600

    onInputChange() function & test

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,13 +1,18 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  // "event" contains a bunch of info about the event that just occured.
+  onInputChange(event) {
+    console.log(event.target.value); // this spec contains the text, that the user just added to input.
+  }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text"></input>
+            <input type="text" onChange={this.onInputChange}></input>
           </div>
         </form>
       </div>
