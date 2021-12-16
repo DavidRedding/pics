66520bf2c2cf4b1efc934e5caaf33f8de9ec2dea
David Redding <redding.david35@gmail.com>
2021-12-16 07:21:14 -0600

    routed axios to unsplash following unsplash doc.

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,16 @@ import axios from "axios";
 import SearchBar from "../SearchBar";
 
 class App extends React.Component {
+  // prettier-ignore
   onSearchSubmit = (term) => {
-    console.log(term);
-  };
+    axios 
+      .get("https://api.unsplash.com/search/photos", {
+        params: { query: term },
+        headers: {
+          Authorization: "Client-ID _u2soxYwIjernz7ogJlXafjkq-Sk531f0JHpVMSB7oE",
+        },
+      })
+    };
 
   render() {
     return (
