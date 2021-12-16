31a1f8186b096cb5105d31e15fc64dee4e94bc75
David Redding <redding.david35@gmail.com>
2021-12-16 10:07:32 -0600

    assign key

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ImageList = (props) => {
-  const images = props.images.map((image) => <img src={image.urls.regular} />);
+  const images = props.images.map((image) => {
+    return (
+      <img key={image.id} src={image.urls.regular} alt={image.description} />
+    );
+  });
 
   return <div>{images}</div>;
 };
