78b027b5cf9773b6554fbf16cbc72eb6f0bbdacc
David Redding <redding.david35@gmail.com>
2021-12-16 13:53:29 -0600

    refactor/destruct image list

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
 import React from "react";
+// prettier-ignore
 
-const ImageList = (props) => {
-  const images = props.images.map((image) => {
-    return (
-      <img key={image.id} src={image.urls.regular} alt={image.description} />
-    );
+const ImageList = (props) => {          
+                                         // image's
+  const images = props.images.map(({ id, urls, description }) => {
+    return <img key={id} src={urls.regular} alt={description} />;
+               //image.id   //image.urls    //image.description
   });
 
   return <div>{images}</div>;
