f79912316afbd9a715840cd0af946d203a61ed92
David Redding <redding.david35@gmail.com>
2021-12-19 08:43:11 -0600

    update Image Card

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -6,7 +6,7 @@ import ImageCard from "./ImageCard";
 const ImageList = (props) => {          
                                          // image's
   const images = props.images.map( image => {
-    return <ImageCard key={image.id} image = {images}/>;
+    return <ImageCard key={image.id} image = {image}/>;
   });
 
   return <div className = "image-list">{images}</div>;
