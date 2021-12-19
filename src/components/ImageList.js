67a8cc2c4847e11b0c6df08a8a9bd61c7d9b8685
David Redding <redding.david35@gmail.com>
2021-12-19 07:07:53 -0600

    Css initailization

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./ImageList.css";
 // prettier-ignore
 
 const ImageList = (props) => {          
@@ -8,7 +9,7 @@ const ImageList = (props) => {
                //image.id   //image.urls    //image.description
   });
 
-  return <div>{images}</div>;
+  return <div className = "image-list">{images}</div>;
 };
 
 export default ImageList;
